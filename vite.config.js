0fe070f2b12fe69a3622edbc7b9e2a09efb9b5ca
Aman Thakur <amanrt360@gmail.com>
2024-06-04 14:30:35 +0530

    Hello

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",
+  plugins: [react()],
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: "index.html", // Ensure it points to your index.html file
+    },
   },
+  base: "./", // Adjust the base path if needed
 });
